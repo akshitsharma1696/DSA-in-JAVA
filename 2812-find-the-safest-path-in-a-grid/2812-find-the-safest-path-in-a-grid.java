d15fd0ec18ae6a66247a8bc8a9a1119ba7553419
Akshit Sharma <akshitsharma1696@gmail.com>
2026-07-01 22:50:49 +0530

    Time: 183 ms (32.7%), Space: 127.8 MB (15.87%) - LeetHub

---
+++ 2812-find-the-safest-path-in-a-grid/2812-find-the-safest-path-in-a-grid.java
@@ -0,0 +1,87 @@
+class Solution {
+    public int maximumSafenessFactor(List<List<Integer>> grid) {
+
+        int n = grid.size();
+
+        int[][] dist = new int[n][n];
+
+        for (int[] row : dist)
+            Arrays.fill(row, -1);
+
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid.get(i).get(j) == 1) {
+                    queue.offer(new int[]{i, j});
+                    dist[i][j] = 0;
+                }
+            }
+        }
+
+        int[][] dir = {
+                {1, 0},
+                {-1, 0},
+                {0, 1},
+                {0, -1}
+        };
+        while (!queue.isEmpty()) {
+
+            int[] cur = queue.poll();
+
+            int r = cur[0];
+            int c = cur[1];
+
+            for (int[] d : dir) {
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n && dist[nr][nc] == -1) {
+
+                    dist[nr][nc] = dist[r][c] + 1;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+                (a, b) -> b[2] - a[2]
+        );
+
+        boolean[][] visited = new boolean[n][n];
+
+        pq.offer(new int[]{0, 0, dist[0][0]});
+
+        while (!pq.isEmpty()) {
+
+            int[] cur = pq.poll();
+
+            int r = cur[0];
+            int c = cur[1];
+            int safe = cur[2];
+
+            if (visited[r][c])
+                continue;
+
+            visited[r][c] = true;
+
+            if (r == n - 1 && c == n - 1)
+                return safe;
+
+            for (int[] d : dir) {
+
+                int nr = r + d[0];
+                int nc = c + d[1];
+
+                if (nr >= 0 && nc >= 0 && nr < n && nc < n && !visited[nr][nc]) {
+
+                    int newSafe = Math.min(safe, dist[nr][nc]);
+
+                    pq.offer(new int[]{nr, nc, newSafe});
+                }
+            }
+        }
+
+        return 0;
+    }
+}
